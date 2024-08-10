3e7f1bed07362e52f5d598d1a914b768e5ae145d
lighttiger2505 <lighttiger2505@gmail.com>
2024-08-10 13:24:20 +0900

    feat(mylinter): add check for OfficeID with personal_data tag
    This commit introduces a new linting rule in mylinter. The rule checks
    if a struct field tagged with `security:"personal_data"` also includes
    an `OfficeID` field of integer type (int, int32, int64, uint, uint32,
    uint64). If the `OfficeID` field is missing or is not of the correct
    type, the linter will report an error. This helps enforce data
    security policies in the codebase.

--- mylinter.go
+++ mylinter.go
@@ -2,39 +2,68 @@ package mylinter
 
 import (
 	"go/ast"
+	"go/types"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
-	"golang.org/x/tools/go/ast/inspector"
 )
-
-const doc = "mylinter is ..."
 
 // Analyzer is ...
 var Analyzer = &analysis.Analyzer{
 	Name: "mylinter",
-	Doc:  doc,
+	Doc:  "checks if OfficeID is present when security tag is `personal_data`",
 	Run:  run,
 	Requires: []*analysis.Analyzer{
 		inspect.Analyzer,
 	},
 }
 
-func run(pass *analysis.Pass) (any, error) {
-	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-
-	nodeFilter := []ast.Node{
-		(*ast.Ident)(nil),
+func run(pass *analysis.Pass) (interface{}, error) {
+	for _, file := range pass.Files {
+		ast.Inspect(file, func(n ast.Node) bool {
+			if structType, ok := n.(*ast.StructType); ok {
+				checkStruct(pass, structType)
+			}
+			return true
+		})
 	}
+	return nil, nil
+}
 
-	inspect.Preorder(nodeFilter, func(n ast.Node) {
-		switch n := n.(type) {
-		case *ast.Ident:
-			if n.Name == "gopher" {
-				pass.Reportf(n.Pos(), "identifier is gopher")
+func isInt(kind types.BasicKind) bool {
+	switch kind {
+	case types.Int, types.Int32, types.Int64, types.Uint, types.Uint32, types.Uint64:
+		return true
+	default:
+		return false
+	}
+}
+
+func checkStruct(pass *analysis.Pass, structType *ast.StructType) {
+	hasPersonalDataTag := false
+	hasOfficeID := false
+
+	for _, field := range structType.Fields.List {
+		if len(field.Names) == 0 {
+			continue
+		}
+		fieldName := field.Names[0].Name
+
+		if field.Tag != nil {
+			tag := field.Tag.Value
+			if tag == "`security:\"personal_data\"`" {
+				hasPersonalDataTag = true
 			}
 		}
-	})
 
-	return nil, nil
+		if fieldName == "OfficeID" {
+			if typ, ok := pass.TypesInfo.TypeOf(field.Type).(*types.Basic); ok && isInt(typ.Kind()) {
+				hasOfficeID = true
+			}
+		}
+	}
+
+	if hasPersonalDataTag && !hasOfficeID {
+		pass.Reportf(structType.Pos(), "struct with `personal_data` tag must have `OfficeID` of type uint")
+	}
 }
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,43 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+type StringOfficeIDStruct struct {
+	// Fail
+	OfficeID   string
+	DriverName string `security:"personal_data"`
+}
+
+type UnmatchOfficeIDStruct struct {
+	// Fail
+	OfficeI    uint
+	DriverName string `security:"personal_data"`
+}
+
+type UintOfficeIDStruct struct {
+	OfficeID   uint
+	DriverName string `security:"personal_data"`
+}
+
+type Uint32OfficeIDStruct struct {
+	OfficeID   uint32
+	DriverName string `security:"personal_data"`
+}
+
+type Uint64OfficeIDStruct struct {
+	OfficeID   uint64
+	DriverName string `security:"personal_data"`
+}
+
+type IntOfficeIDStruct struct {
+	OfficeID   int
+	DriverName string `security:"personal_data"`
+}
+
+type Int32OfficeIDStruct struct {
+	OfficeID   int32
+	DriverName string `security:"personal_data"`
+}
+
+type Int64OfficeIDStruct struct {
+	OfficeID   int64
+	DriverName string `security:"personal_data"`
 }
